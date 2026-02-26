b3292312c9615a47e38969f78c6a9f7ccc8dd2f7
shamshad-dev27 <shamshadahmad843127@gmail.com>
2026-02-26 13:07:50 +0530

    Started adding redux setup with toolkit

---
+++ src/Redux/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authSliceReducer from './Slies/AuthSlice.js'
+const store=configureStore({
+    reducer:{
+        auth:authSliceReducer,
+    },
+    devTools:true,
+});
+export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import './index.css'
 
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx'
+import store from './Redux/Slices/store.js';
 
 createRoot(document.getElementById('root')).render(
+   <Provider store={store}>
    <BrowserRouter>
     <Toaster position="top-right" />
     <App />
    </BrowserRouter>
+   </Provider>
 )
