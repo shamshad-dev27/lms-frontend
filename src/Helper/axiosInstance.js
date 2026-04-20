8991c20ea0314b30ceb3c5a55c0ac76e8b66c272
shamshad-dev27 <shamshadahmad843127@gmail.com>
2026-04-20 20:55:14 +0530

    axios

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://lms-backend-29dk.onrender.com/api/v1';
+const BASE_URL = 'https://lms-backend-29dk.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
