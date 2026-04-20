d58171c15ed7ffacc6f6c34db181a09b172934c1
shamshad-dev27 <shamshadahmad843127@gmail.com>
2026-04-20 20:33:25 +0530

    lecture

--- src/Redux/Slices/LectureSlice.js
+++ src/Redux/Slices/LectureSlice.js
@@ -71,10 +71,10 @@ const LectrueSlice=createSlice({
     reducers:{},
     extraReducers:(builder)=>{
             builder.addCase(getCourseLecture.fulfilled,(state,action)=>{
-                state.lecture=action?.payload?.lectures||action?.payload?.course?.lectures;
+                state.lecture = action?.payload?.lectures || action?.payload?.course?.lectures || [];
             })
             .addCase(AddCourseLecture.fulfilled,(state,action)=>{
-                  state.lecture=action?.payload?.course?.lecture;
+                 state.lecture = action?.payload?.course?.lectures || [];
             })
     }
 })
